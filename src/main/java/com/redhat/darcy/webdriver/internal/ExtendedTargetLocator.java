110c5eb511a4909b9dd7189da22c3767257adef0
Alec Henninger <alechenninger@gmail.com>
2014-05-22 09:48:54 -0400

    Remove unused interface

--- src/main/java/com/redhat/darcy/webdriver/internal/ExtendedTargetLocator.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.darcy.webdriver.internal;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriver.TargetLocator;
-
-public interface ExtendedTargetLocator extends TargetLocator {
-    WebDriver nested(WebDriverTarget parent, WebDriverTarget child);
-}
