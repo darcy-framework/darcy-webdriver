17b4822b999f46b5132e97e1809777304226e13b
Alec Henninger <alechenninger@gmail.com>
2014-06-17 21:16:00 -0400

    Correct DefaultWebDriverElementContext behavior (fixes #2)

--- src/main/java/com/redhat/darcy/webdriver/internal/DefaultWebDriverElementContext.java
+++ src/main/java/com/redhat/darcy/webdriver/internal/DefaultWebDriverElementContext.java
@@ -23,131 +23,324 @@ import com.redhat.darcy.DarcyException;
 import com.redhat.darcy.ui.Locator;
 import com.redhat.darcy.ui.elements.Element;
 import com.redhat.darcy.webdriver.WebDriverElementContext;
-import com.redhat.darcy.webdriver.elements.WebDriverElement;
 import com.redhat.darcy.webdriver.locators.ByPartialVisibleText;
 import com.redhat.darcy.webdriver.locators.ByVisibleText;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.WrapsElement;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class DefaultWebDriverElementContext implements WebDriverElementContext {
-    private final SearchContext context;
+    private final WebElementContext context;
     private final ElementFactory elementFactory;
-    
+
     public DefaultWebDriverElementContext(SearchContext context, ElementFactory elementFactory) {
-        this.context = context;
+        this.context = new WebElementContext(context);
         this.elementFactory = elementFactory;
     }
-    
+
     @Override
     public <T> List<T> findAllById(Class<T> type, String id) {
-        return findElements(type, By.id(id));
+        return newElementList(type, () -> context.findAllById(WebElement.class, id));
     }
-    
+
     @Override
     public <T> T findById(Class<T> type, String id) {
-        return findElement(type, By.id(id));
+        return newElement(type, () -> context.findById(WebElement.class, id));
     }
-    
+
     @Override
     public <T> List<T> findAllByName(Class<T> type, String name) {
-        return findElements(type, By.name(name));
+        return newElementList(type, () -> context.findAllByName(WebElement.class, name));
     }
-    
+
+    @Override
+    public <T> T findByName(Class<T> type, String name) {
+        return newElement(type, () -> context.findByName(WebElement.class, name));
+    }
+
     @Override
     public <T> List<T> findAllByLinkText(Class<T> type, String linkText) {
-        return findElements(type, By.linkText(linkText));
+        return newElementList(type, () -> context.findAllByLinkText(WebElement.class, linkText));
     }
-    
+
+    @Override
+    public <T> T findByLinkText(Class<T> type, String linkText) {
+        return newElement(type, () -> context.findByLinkText(WebElement.class, linkText));
+    }
+
     @Override
     public <T> List<T> findAllByTextContent(Class<T> type, String textContent) {
-        return findElements(type, new ByVisibleText(textContent));
+        return newElementList(type,
+                () -> context.findAllByTextContent(WebElement.class, textContent));
     }
-    
+
+    @Override
+    public <T> T findByTextContent(Class<T> type, String textContent) {
+        return newElement(type, () -> context.findByTextContent(WebElement.class, textContent));
+    }
+
     @Override
     public <T> List<T> findAllByPartialTextContent(Class<T> type, String partialTextContent) {
-        return findElements(type, new ByPartialVisibleText(partialTextContent));
+        return newElementList(type,
+                () -> context.findAllByPartialTextContent(WebElement.class, partialTextContent));
     }
-    
+
+    @Override
+    public <T> T findByPartialTextContent(Class<T> type, String partialTextContent) {
+        return newElement(type,
+                () -> context.findByPartialTextContent(WebElement.class, partialTextContent));
+    }
+
     @Override
     public <T> List<T> findAllByXPath(Class<T> type, String xpath) {
-        return findElements(type, By.xpath(xpath));
+        return newElementList(type, () -> context.findAllByXPath(WebElement.class, xpath));
     }
-    
+
+    @Override
+    public <T> T findByXPath(Class<T> type, String xpath) {
+        return newElement(type, () -> context.findByXPath(WebElement.class, xpath));
+    }
+
     @Override
     public <T> List<T> findAllByCssSelector(Class<T> type, String css) {
-        return findElements(type, By.cssSelector(css));
+        return newElementList(type, () -> context.findAllByCssSelector(WebElement.class, css));
     }
-    
+
+    @Override
+    public <T> T findByCssSelector(Class<T> type, String css) {
+        return newElement(type, () -> context.findByCssSelector(WebElement.class, css));
+    }
+
     @Override
     public <T> List<T> findAllByHtmlTag(Class<T> type, String tag) {
-        return findElements(type, By.tagName(tag));
+        return newElementList(type, () -> context.findAllByHtmlTag(WebElement.class, tag));
     }
-    
+
+    @Override
+    public <T> T findByHtmlTag(Class<T> type, String tag) {
+        return newElement(type, () -> context.findByHtmlTag(WebElement.class, tag));
+    }
+
     @Override
     public <T> List<T> findAllByChained(Class<T> type, Locator... locators) {
-        if (locators.length == 0) {
-            return new ArrayList<>(0);
-        }
-        
-        List<T> elements = null;
-        List<T> subElements = new LinkedList<>();
-        
-        for (Locator locator : locators) {
-            if (elements == null) {
-                elements = locator.findAll(type, this);
-            } else {
-                for (T element : elements) {
-                    subElements.addAll(
-                            locator.findAll(
-                                    type,
-                                    new DefaultWebDriverElementContext(
-                                            ((WebDriverElement) element).getWrappedElement(),
-                                            elementFactory)));
-                }
-                
-                elements.clear();
-                elements.addAll(subElements);
-                subElements.clear();
-            }
-        }
-        
-        return elements;
+        return newElementList(type, () -> context.findAllByChained(WebElement.class, locators));
     }
-    
+
+    @Override
+    public <T> T findByChained(Class<T> type, Locator... locators) {
+        return newElement(type, () -> context.findByChained(WebElement.class, locators));
+    }
+
     @Override
     public <T> List<T> findAllByNested(Class<T> type, Element parent, Locator child) {
-        if (!(parent instanceof WebDriverElement)) {
-            throw new DarcyException("Cannot cast element to WebDriverElement. Is the parent "
-                    + "from another automation library? " + parent.toString());
-        }
-        
-        return child.findAll(type, new DefaultWebDriverElementContext(
-                ((WebDriverElement) parent).getWrappedElement(), elementFactory));
+        return newElementList(type, () -> context.findAllByNested(WebElement.class, parent, child));
     }
-    
-    @SuppressWarnings("unchecked")
-    private <T> T findElement(Class<T> type, By by) {
-        if (!Element.class.isAssignableFrom(type)) {
-            throw new DarcyException("An ElementContext can only locate Element types: " 
-                    + type.toString());
-        }
-        
-        return (T) elementFactory.newElement((Class<Element>) type, () -> by.findElement(context));
+
+    @Override
+    public <T> T findByNested(Class<T> type, Element parent, Locator child) {
+        return newElement(type, () -> context.findByNested(WebElement.class, parent, child));
     }
-  
+
     @SuppressWarnings("unchecked")
-    private <T> List<T> findElements(Class<T> type, By by) {
+    private <T> T newElement(Class<T> type, Supplier<WebElement> source) {
         if (!Element.class.isAssignableFrom(type)) {
-            throw new DarcyException("An ElementContext can only locate Element types: " 
+            throw new DarcyException("An ElementContext can only locate Element types: "
                     + type.toString());
         }
 
-        return (List<T>) elementFactory.newElementList(
-                (Class<Element>) type, () -> by.findElements(context));
+        return (T) elementFactory.newElement((Class<Element>) type, source);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> List<T> newElementList(Class<T> type, Supplier<List<WebElement>> source) {
+        if (!Element.class.isAssignableFrom(type)) {
+            throw new DarcyException("An ElementContext can only locate Element types: "
+                    + type.toString());
+        }
+
+        return (List<T>) elementFactory.newElementList((Class<Element>) type, source);
+    }
+
+    /**
+     * Simply converts from Locators to WebElements, or Lists thereof. Meant to be lazily called
+     * when actually needing to find an element to work with.
+     * <p>
+     * <strong>This class only finds WebElements by design. Trying to find other types will throw a
+     * class cast exception.</strong>
+     * <p>
+     * This class implements WebDriverElementContext so that it can be used with chained locators.
+     * See {@link #findAllByChained(Class, com.redhat.darcy.ui.Locator...)}.
+     */
+    private class WebElementContext implements WebDriverElementContext {
+        private final SearchContext sc;
+
+        WebElementContext(SearchContext sc) {
+            this.sc = sc;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByChained(Class<T> type, Locator... locators) {
+            List<WebElement> elements = null;
+            List<WebElement> subElements = new LinkedList<>();
+
+            for (Locator locator : locators) {
+                if (elements == null) {
+                    elements = locator.findAll(WebElement.class, this);
+                } else {
+                    for (WebElement element : elements) {
+                        subElements.addAll(
+                                locator.findAll(WebElement.class, new WebElementContext(element)));
+                    }
+
+                    elements.clear();
+                    elements.addAll(subElements);
+                    subElements.clear();
+                }
+            }
+
+            return (List<T>) elements;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByChained(Class<T> type, Locator... locators) {
+            List<WebElement> elements = findAllByChained(WebElement.class, locators);
+
+            if (elements.isEmpty()) {
+                throw new NoSuchElementException("No elements found. SearchContext: '" + sc
+                        + "', Locators: " + Arrays.toString(locators));
+            }
+
+            return (T) elements.get(0);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByCssSelector(Class<T> type, String css) {
+            return (List<T>) By.cssSelector(css).findElements(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByCssSelector(Class<T> type, String css) {
+            return (T) By.cssSelector(css).findElement(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByHtmlTag(Class<T> type, String tag) {
+            return (List<T>) By.tagName(tag).findElements(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByHtmlTag(Class<T> type, String tag) {
+            return (T) By.tagName(tag).findElement(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllById(Class<T> type, String id) {
+            return (List<T>) By.id(id).findElements(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findById(Class<T> type, String id) {
+            return (T) By.id(id).findElement(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByLinkText(Class<T> type, String linkText) {
+            return (List<T>) By.linkText(linkText).findElements(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByLinkText(Class<T> type, String linkText) {
+            return (T) By.linkText(linkText).findElement(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByName(Class<T> type, String name) {
+            return (List<T>) By.name(name).findElements(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByName(Class<T> type, String name) {
+            return (T) By.name(name).findElement(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByNested(Class<T> type, Element parent, Locator child) {
+            if (!(parent instanceof WrapsElement)) {
+                throw new DarcyException("Parent element does not wrap a WebElement. Is this "
+                        + "element from another automation library?");
+            }
+
+            return (List<T>) child.findAll(WebElement.class,
+                    new WebElementContext(((WrapsElement) parent).getWrappedElement()));
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByNested(Class<T> type, Element parent, Locator child) {
+            if (!(parent instanceof WrapsElement)) {
+                throw new DarcyException("Parent element does not wrap a WebElement. Is this "
+                        + "element from another automation library?");
+            }
+
+            return (T) child.find(WebElement.class,
+                    new WebElementContext(((WrapsElement) parent).getWrappedElement()));
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByPartialTextContent(Class<T> type, String partialTextContent) {
+            return (List<T>) new ByPartialVisibleText(partialTextContent).findElements(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByPartialTextContent(Class<T> type, String partialTextContent) {
+            return (T) new ByPartialVisibleText(partialTextContent).findElement(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByTextContent(Class<T> type, String textContent) {
+            return (List<T>) new ByVisibleText(textContent).findElements(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByTextContent(Class<T> type, String textContent) {
+            return (T) new ByVisibleText(textContent).findElement(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> findAllByXPath(Class<T> type, String xpath) {
+            return (List<T>) By.xpath(xpath).findElements(sc);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T findByXPath(Class<T> type, String xpath) {
+            return (T) By.xpath(xpath).findElement(sc);
+        }
     }
 }
