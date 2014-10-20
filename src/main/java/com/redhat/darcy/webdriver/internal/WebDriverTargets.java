874097f8d0242061ea49fe43cb4d9343b8b84c8a
Alec Henninger <alechenninger@gmail.com>
2014-10-20 12:20:54 -0400

    Remove unused import

--- src/main/java/com/redhat/darcy/webdriver/internal/WebDriverTargets.java
+++ src/main/java/com/redhat/darcy/webdriver/internal/WebDriverTargets.java
@@ -19,7 +19,6 @@
 
 package com.redhat.darcy.webdriver.internal;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver.TargetLocator;
 import org.openqa.selenium.WebElement;
