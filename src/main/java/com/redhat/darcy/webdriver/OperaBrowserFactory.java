771105ab659d0c66c1892fbfeafa26fe085eaa1b
abocz <aboczjr@gmail.com>
2014-11-12 20:49:32 -0500

    Add OperaDriverFactory and OperaPrestoDriver to the pom

---
+++ src/main/java/com/redhat/darcy/webdriver/OperaBrowserFactory.java
@@ -0,0 +1,58 @@
+package com.redhat.darcy.webdriver;
+
+import com.opera.core.systems.OperaDriver;
+import com.opera.core.systems.OperaProfile;
+import com.opera.core.systems.OperaSettings;
+import com.redhat.darcy.web.api.Browser;
+import com.redhat.darcy.webdriver.elements.WebDriverElement;
+import org.openqa.selenium.Capabilities;
+
+/**
+ * Fluently describes a {@link com.redhat.darcy.web.api.BrowserFactory} that creates specifically
+ * configured {@link com.opera.core.systems.OperaDriver}.
+ */
+public class OperaBrowserFactory extends WebDriverBrowserFactory<OperaBrowserFactory> {
+    private Capabilities capabilities;
+    private OperaProfile profile;
+    private OperaSettings settings;
+    private ElementConstructorMap elementImpls = ElementConstructorMap
+            .newElementConstructorMapWithDefaults();
+
+    @Override
+    public Browser newBrowser() {
+        OperaDriver driver;
+
+        if (profile != null) {
+            driver = new OperaDriver(profile);
+        } else if (capabilities != null) {
+            driver = new OperaDriver(capabilities);
+        } else if (settings != null) {
+            driver = new OperaDriver(settings);
+        } else {
+            driver = new OperaDriver();
+        }
+        return makeBrowser(driver, elementImpls);
+    }
+
+    public OperaBrowserFactory capableOf(Capabilities cap) {
+        capabilities = cap;
+        return this;
+    }
+
+    public OperaBrowserFactory usingOptions(OperaProfile prf) {
+        profile = prf;
+        return this;
+    }
+
+    public OperaBrowserFactory usingService(OperaSettings set) {
+        settings = set;
+        return this;
+    }
+
+    @Override
+    public <E extends WebDriverElement> OperaBrowserFactory withElementImplementation(
+            Class<? super E> type, ElementConstructor<E> constructor) {
+        elementImpls.put(type, constructor);
+        return this;
+    }
+}
---
+++ src/test/java/com/redhat/darcy/webdriver/OperaBrowserFactoryTest.java
@@ -0,0 +1,7 @@
+package com.redhat.darcy.webdriver;
+
+/**
+ * Created by Andrew on 11/12/2014.
+ */
+public class OperaBrowserFactoryTest {
+}
