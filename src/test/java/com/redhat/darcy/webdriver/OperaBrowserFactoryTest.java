ff98794cac42bcf082418d1aa3b9a8cddb728c9d
abocz <aboczjr@gmail.com>
2014-11-13 21:38:27 -0500

    add OperaBrowserFactoryTest

--- src/test/java/com/redhat/darcy/webdriver/OperaBrowserFactoryTest.java
+++ src/test/java/com/redhat/darcy/webdriver/OperaBrowserFactoryTest.java
@@ -1,7 +1,28 @@
 package com.redhat.darcy.webdriver;
 
-/**
- * Created by Andrew on 11/12/2014.
- */
+import com.opera.core.systems.OperaDriver;
+import com.redhat.darcy.webdriver.internal.CachingTargetLocator;
+import com.redhat.darcy.webdriver.internal.ForwardingTargetedWebDriver;
+import com.redhat.darcy.webdriver.testing.rules.TraceTestName;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
 public class OperaBrowserFactoryTest {
+    @Rule
+    public TraceTestName traceTestName = new TraceTestName();
+
+    @Test
+    public void shouldBeInstanceOfUntargetedChromeDriver() {
+        WebDriverBrowserFactory browserFactory = new OperaBrowserFactory();
+
+        WebDriverBrowser browser = (WebDriverBrowser) browserFactory.newBrowser();
+        ForwardingTargetedWebDriver webDriver = (ForwardingTargetedWebDriver) browser.getWrappedDriver();
+        CachingTargetLocator targetLocator = (CachingTargetLocator) webDriver.getTargetLocator();
+
+        assertThat(targetLocator.getUntargetedDriver(), instanceOf(OperaDriver.class));
+        browser.close();
+    }
 }
