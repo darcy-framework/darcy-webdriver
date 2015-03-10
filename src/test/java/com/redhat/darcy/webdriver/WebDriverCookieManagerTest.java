2a336f88bea859660d94edc1d27cc6db73623d61
Andrew Bocz <abocz@redhat.com>
2015-03-10 15:31:00 -0400

    Clean up imports

--- src/test/java/com/redhat/darcy/webdriver/WebDriverCookieManagerTest.java
+++ src/test/java/com/redhat/darcy/webdriver/WebDriverCookieManagerTest.java
@@ -1,16 +1,29 @@
+/*
+ Copyright 2014 Red Hat, Inc. and/or its affiliates.
+
+ This file is part of darcy-webdriver.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.redhat.darcy.webdriver;
 
-import static org.hamcrest.Matchers.instanceOf;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 import com.redhat.darcy.web.Cookie;
-import com.redhat.darcy.web.api.Browser;
 import com.redhat.darcy.webdriver.internal.TargetedWebDriver;
 import com.redhat.darcy.webdriver.internal.WebDriverCookieManager;
-import com.redhat.darcy.webdriver.testing.doubles.StubWebDriverElementContext;
-import com.redhat.darcy.webdriver.testing.doubles.StubWebDriverParentContext;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
