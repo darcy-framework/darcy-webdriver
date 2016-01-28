7dc5d1064bdf3a295980203a6c7bcee4d1332fcd
Andrew Bocz <abocz@redhat.com>
2016-01-28 11:22:41 -0500

    Fix error in TargetedWebElementTest

--- src/test/java/com/redhat/darcy/webdriver/internal/TargetedWebElementTest.java
+++ src/test/java/com/redhat/darcy/webdriver/internal/TargetedWebElementTest.java
@@ -5,22 +5,23 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
+@RunWith(JUnit4.class)
 public class TargetedWebElementTest {
 
     @Test
     public void shouldCallGetRectOnUnderlyingWebElement() {
-        WebDriver.TargetLocator targetLocator = mock(WebDriver.TargetLocator.class);
-        WebDriverTarget webDriverTarget = mock(WebDriverTarget.class);
-        WebElement webElement = mock(WebElement.class);
-        when(webElement.getRect()).thenReturn(mock(Rectangle.class));
-        TargetedWebElement targetedWebElement = new TargetedWebElement(targetLocator, webDriverTarget, webElement);
+        WebDriver.TargetLocator locator = mock(WebDriver.TargetLocator.class);
+        WebDriverTarget target = mock(WebDriverTarget.class);
+        TargetedWebElement element = mock(TargetedWebElement.class);
+        when(element.getRect()).thenReturn(mock(Rectangle.class));
+        TargetedWebElement targetedWebElement = new TargetedWebElement(locator, target, element);
 
         targetedWebElement.getRect();
-        verify(webElement.getRect());
+        verify(element).getRect();
     }
-
 }
